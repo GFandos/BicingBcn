faa58fe9296162cd36c4dff31d8bffabef1573ac
gfandos <gerard.fandos@gmail.com>
2017-01-31 18:12:00 +0100

    Initial commit

---
+++ app/src/main/java/gfandos/bicingbcn/Pojo/Station.java
@@ -0,0 +1,45 @@
+package gfandos.bicingbcn.Pojo;
+
+/**
+ * Created by 47989768s on 31/01/17.
+ */
+
+public class Station {
+
+    private String id;
+    private String name;
+    private String latitude;
+    private String longitude;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+}
